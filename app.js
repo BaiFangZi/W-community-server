bf17088f2354920bc77a31aec4dd8cd04b840757
BaiFangZi <2739666469@qq.com>
2020-08-24 20:02:06 +0800

    首页板块内容接口

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express')
 const router = require('./routes')
 var history = require('connect-history-api-fallback');
 var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
 
 const app = express()
 app.use(history());
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({
 	extended: false
 }))
 app.use(bodyParser.json())
+app.use(cookieParser('9826zxx'))
 app.use(router)
 // app.post('/api/v1/login', async (req, res) => {
 // 	console.log(req.body)
--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ router.post('/api/v1/login', function(req, res) {
 			return false;
 		}
 		const db = client.db('wCommunity');
-		db.collection('users').findOne({
+		db.collection('user').findOne({
 			username: req.body.username
 		}, (error, result) => {
 			if (error) { //链接出错
@@ -22,6 +22,13 @@ router.post('/api/v1/login', function(req, res) {
 				return false;
 			} else { //判断是否有此用户，有的话判断密码是否一致
 				if (result && result.password === req.body.password) {
+					// console.log(result._id)
+					// res.cookie('role', result.username, {
+					// 	maxAge: 1000 * 60 * 60 * 24,
+					// 	// signed: true,
+					// 	// httpOnly: true,
+					// 	// domain:'',//域名
+					// });
 					res.send({
 						mes: '登陆成功',
 						code: 0,
@@ -46,13 +53,84 @@ router.post('/api/v1/regist', (req, res) => {
 			return false;
 		}
 		const db = client.db('wCommunity');
-		db.collection('users').insertOne(req.body, (error, result) => {
+		db.collection('user').insertOne(req.body, (error, result) => {
 			if (error) {
 				console.log("增加数据失败");
 				return false;
 			}
 			res.send("增加数据成功");
 			client.close();
+		})
+	});
+})
+//获取最新文章
+router.get('/api/v1/artical/latest', (req, res) => {
+	MongoClient.connect(url, (err, client) => {
+		if (err) {
+			console.log("数据库连接失败");
+			return false;
+		}
+		const db = client.db('wCommunity');
+		db.collection('articalInfo').find({}, {
+			projection: {
+				title: 1
+			}
+		}).limit(20).toArray((err, result) => {
+			if (err)
+				throw err
+			res.json({
+				msg: '成功',
+				data: result
+			})
+			client.close()
+		})
+
+	});
+})
+
+//获取最新问答
+router.get('/api/v1/problem/latest', (req, res) => {
+	MongoClient.connect(url, (err, client) => {
+		if (err) {
+			console.log("数据库连接失败");
+			return false;
+		}
+		const db = client.db('wCommunity');
+		db.collection('problemInfo').find({}, {
+			projection: {
+				title: 1
+			}
+		}).limit(20).toArray((err, result) => {
+			if (err)
+				throw err
+			res.json({
+				msg: '成功',
+				data: result
+			})
+			client.close()
+		})
+	});
+})
+
+//发布文章
+
+router.post('/api/v1/artical/create', (req, res) => {
+	MongoClient.connect(url, (err, client) => {
+		if (err) {
+			console.log("数据库连接失败");
+			return false;
+		}
+		const db = client.db('wCommunity');
+		db.collection('artical').insertOne({
+			title: req.body.title,
+			content: req.body.content
+		}, (err, result) => {
+			if (err)
+				throw err
+			res.json({
+				msg: '成功',
+			})
+			client.close()
 		})
 	});
 })
